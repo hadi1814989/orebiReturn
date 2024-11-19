ca7eb3a68737b98751b877e6a851cf91daf5aa2d
hadi1814989 <freelancer1814989@gmail.comgit config --global user.email freelancer1814989@gmail.com>
2024-11-19 13:05:33 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextApi from './components/Contextapi.jsx'
+import{ ContextApi} from './components/Contextapi.jsx'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <ContextApi>
